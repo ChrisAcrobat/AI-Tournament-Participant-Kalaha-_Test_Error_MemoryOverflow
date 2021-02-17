7d99bd973e4ebe216227c3065aada488d71b8d43
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-02-17 08:53:32 +0100

    Update participant.js

--- participant.js
+++ participant.js
@@ -2,18 +2,17 @@
 let sleep;
 let a = Math.PI.toString();
 setInterval(()=>a+=a, 1);
-function tock(messageEvent){
+ParticipantHelper.init = settings => {
+	sleep = settings.general.timelimit_ms*.9;
+}
+ParticipantHelper.onmessage = data => {
 	setTimeout(()=>{
-		let length = (messageEvent.data.length/2) - 1;
+		let length = (data.length/2) - 1;
 		for(let i = 0; 0 < length; i++){
-			if(0 < messageEvent.data[i]){
-				postMessage(i);
+			if(0 < data[i]){
+				ParticipantHelper.respond(i);
 				return;
 			}
 		}
 	},sleep);
 }
-onmessage = messageEvent => {
-	sleep = messageEvent.data.settings.general.timelimit_ms*.9;
-	onmessage = tock;
-};
